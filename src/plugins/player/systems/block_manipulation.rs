82a1f73938eb742451c1388164d42051e2ff2179
ktullavik <ktullavik@gmail.com>
2024-03-03 16:49:45 +0100

    Formatting.

--- src/plugins/player/systems/block_manipulation.rs
+++ src/plugins/player/systems/block_manipulation.rs
@@ -59,14 +59,20 @@ pub fn block_placing_system(
         let origin = camera_transform.translation;
         let direction: Vec3 = *camera_transform.forward();
     
-        if let Some((_, intersection)) = rapier_context.cast_ray_and_get_normal(origin, direction, 10.0, true, QueryFilter::exclude_dynamic()) {
+        if let Some((_, intersection)) = rapier_context.cast_ray_and_get_normal(
+            origin,
+            direction,
+            10.0,
+            true,
+            QueryFilter::exclude_dynamic()) {
+
             let hit = (intersection.point + intersection.normal * 0.5).floor();
             let chunk_pos = ((hit.x / CHUNK_WIDTH as f32).floor() as i32, (hit.z / CHUNK_WIDTH as f32).floor() as i32);
             let (x, y, z) = ((hit.x  - (chunk_pos.0 as f32 * CHUNK_WIDTH as f32)) as usize,
-                                                (hit.y) as usize,
-                                                (hit.z - (chunk_pos.1 as f32 *  CHUNK_WIDTH as f32)) as usize);
+                                                  (hit.y) as usize,
+                                                  (hit.z - (chunk_pos.1 as f32 *  CHUNK_WIDTH as f32)) as usize);
 
-            let index = x + y * CHUNK_WIDTH + z * CHUNK_WIDTH*CHUNK_HEIGHT;
+            let index = x + y * CHUNK_WIDTH + z * CHUNK_WIDTH * CHUNK_HEIGHT;
             if world_map.chunks[&chunk_pos][index] == BlockType::Air
             || world_map.chunks[&chunk_pos][index] == BlockType::Water {
                 world_map.chunks.get_mut(&chunk_pos).unwrap()[index] = BlockType::Stone;
