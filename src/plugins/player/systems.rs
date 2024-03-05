0493f536d6beca03d8716056481e2552a054fd23
ktullavik <ktullavik@gmail.com>
2024-03-05 02:14:36 +0100

    Whitespace.

--- src/plugins/player/systems.rs
+++ src/plugins/player/systems.rs
@@ -1,10 +1,8 @@
 use bevy::{prelude::*, input::mouse::MouseMotion, ecs::event::ManualEventReader};
 
-
-
-
 pub(crate) mod block_manipulation;
 pub(crate) mod player_movement;
+
 
 #[derive(Resource, Default)]
 pub struct InputState {
